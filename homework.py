bfe5fad60b1098967a1a9439ecba9575b864c314
Mirroxum-vent <director.kernelix@yandex.ru>
2022-06-07 10:08:22 +1000

    v4.0

--- homework.py
+++ homework.py
@@ -23,7 +23,7 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_TIME = 5
+RETRY_TIME = 600
 TIMEOUT_SERVER = 5
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
